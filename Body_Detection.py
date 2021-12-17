1139e31cc3ebf4059c578697d397e02f645e6133
Johnsonchan105 <johnsonchan105@gmail.com>
2021-12-16 20:24:45 -0800

    added random files

--- Body_Detection.py
+++ Body_Detection.py
@@ -16,14 +16,14 @@ while True:
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     results = holistic.process(imgRGB)
 
-    mpDraw.draw_landmarks(img, results.face_landmarks,
-                          mpHolistic.FACE_CONNECTIONS, drawing_specs, drawing_specs)
+    # mpDraw.draw_landmarks(img, results.face_landmarks,
+    #                      mpHolistic.FACE_CONNECTIONS, drawing_specs, drawing_specs)
     mpDraw.draw_landmarks(img, results.left_hand_landmarks,
                           mpHolistic.HAND_CONNECTIONS, drawing_specs, drawing_specs)
     mpDraw.draw_landmarks(img, results.right_hand_landmarks,
                           mpHolistic.HAND_CONNECTIONS, drawing_specs, drawing_specs)
-    mpDraw.draw_landmarks(img, results.pose_landmarks,
-                          mpHolistic.POSE_CONNECTIONS, drawing_specs, drawing_specs)
+    # mpDraw.draw_landmarks(img, results.pose_landmarks,
+    #                      mpHolistic.POSE_CONNECTIONS, drawing_specs, drawing_specs)
     cv2.imshow('Image', img)
     if(cv2.waitKey(1) & 0xff == ord('q')):
         break
